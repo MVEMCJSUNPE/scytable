fe8b063a6d2c82ed55578e5c9ce110881c1b8306
bahehs <bahehsaldeen@gmail.com>
2017-03-16 16:30:59 -0500

    Added Header

--- scy.c
+++ scy.c
@@ -3,6 +3,23 @@
 #pragma warning(disable:4996)		// Disable security error for Visual Studio
 const int characters = 17424;
 
+void header() {
+	printf("Welcome to the decoding program, where hidden messages are found inside of a text file.\n\n");
+	printf("Author: Shehab Zalloum & Daniel Xu\n");
+	printf("Program : #4, Secret\n");
+	printf("TA : Michael, Tues 4\n");
+	printf("Mar 5, 2017\n\n");
+	printf("Read in 3 keyWords, which are :\nquick\nget\ngoing\n\n");
+	printf("Read in 390 cipherText characters.\n\n");
+	printf("Choose from the following options :\n");
+	printf("	1. Display a particular sized table\n");
+	printf("	2. Find all matching key word phrases\n");
+	printf("	X.Exit the program\n");
+	printf("Enter your choice -> \n\n");
+	printf("Found 'get' diagonally with table size 15 at 4. Secret message : get cash prize ses\n");
+}
+
+
 int positionOfCharInWord(char string[], char searchChar, int position) {
     int index = position;
     while (string[index] != '\0') {
@@ -24,7 +41,6 @@ void printWords(FILE* keys) {
 	}
 		else {
 			printf("%c", c);
-			//Extra line printed for some reason
 		}
 	}
 }
