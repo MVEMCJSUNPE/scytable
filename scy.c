9bdfa4e4f031d9732861e53b17d9305612bc7f96
MVEMCJSUNPE <2frac.d.x@gmail.com>
2017-03-16 11:31:09 -0500

    Updated promptUser function
    promptUser function now takes a number from the user and prints that number of characters from the array cipher per line.

--- scy.c
+++ scy.c
@@ -2,14 +2,6 @@
 #include <stdlib.h>
 
 const int characters = 17424;
-//const int 
-//void readIn(file);
-int promptUser(char array[characters]) {
-    int number = 0;
-    printf("Please enter a number (1..132): \n");
-    scanf("%d", &number);
-    printf("userInput: %d\n", number);
-}
 
 int positionOfCharInWord(char string[], char searchChar, int position) {
     int index = position;
@@ -22,6 +14,7 @@ int positionOfCharInWord(char string[], char searchChar, int position) {
     return -1;
 }
 
+//Stage 1
 void printWords(FILE* keys) {
     int i = 0;
     char c = '-';
@@ -37,6 +30,7 @@ void printWords(FILE* keys) {
     }
 }
 
+//Stage 2
 //DO NOT USE 17424 in a for loop for this function. Otherwise, it will print characters past '\0'. 
 void readCipher(FILE* cipher, char cipherText[characters]) {
     int i = 0;
@@ -54,6 +48,26 @@ void readCipher(FILE* cipher, char cipherText[characters]) {
     }
 }
 
+//Stage 3
+void promptUser(char cipherText[characters]) {
+    int number = 0;
+    int i = 0;
+    printf("Please enter a number (1..132): \n");
+    scanf("%d", &number);
+    while (cipherText[i] != '\0') {
+	if ((i % number == 0) ) {
+	    printf("\n");
+	    printf("%c", cipherText[i]);
+	    ++i;
+	}
+	else {
+	    printf("%c", cipherText[i]);
+	    ++i;
+	}
+    }
+}
+
+
 int main(void) {
     FILE* keys = NULL;
     keys = fopen("keys.txt", "r");
@@ -70,6 +84,8 @@ int main(void) {
     char cipherText[characters];
     printWords(keys);
     readCipher(cipher, cipherText);
-    
+    promptUser(cipherText);
+    fclose(keys);
+    fclose(cipher);
 
 }
